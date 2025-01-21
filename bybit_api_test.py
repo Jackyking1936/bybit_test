be1142bfb3998fcf6f83a4770287ef999eb57c1e
joukey <j75772763@gmail.com>
2025-01-21 14:13:16 +0800

    first test

--- bybit_api_test.py
+++ bybit_api_test.py
@@ -1,8 +1,22 @@
-#%%
+#%% session establish
 from pybit.unified_trading import HTTP
 session = HTTP(testnet=True)
+
+#%% 取得公告
 print(session.get_announcement(
-    locale="en-US",
+    locale="zh-TW",
     limit=1,
+))
+
+# %% Get Server Time
+print(session.get_server_time())
+
+# %% Get Kline
+print(session.get_kline(
+    category="inverse",
+    symbol="BTCUSD",
+    interval=60,
+    start=1670601600000,
+    end=1670608800000,
 ))
 # %%
